fdd5186fa4c41577cb9d64de4c9fbece2628d9ed
ThomasStevens <58775290+thomas-stevens@users.noreply.github.com>
2020-02-07 13:03:18 +0100

    Eerste versie (Studenthuis)
    Goed begin gemaakt en nu bezig met de accordion.

--- opdracht1/js/script.js
+++ opdracht1/js/script.js
@@ -4,3 +4,20 @@
   2. Koppel een evenListener aan het element om een mouse-event te detecteren
   3. Gebruik het Classlist object om een css class aan een element toe te voegen of weg te halen.
 */
+
+//Bron: https://www.w3schools.com/howto/howto_js_accordion.asp
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
