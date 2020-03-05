28c06b31f12261e023ebc5a48285d017eb55e8c2
ThomasStevens <58775290+thomas-stevens@users.noreply.github.com>
2020-03-05 14:09:49 +0100

    Update script.js

--- opdracht2/js/script.js
+++ opdracht2/js/script.js
@@ -1,10 +1,10 @@
 window.addEventListener('keydown', (e) => {
   console.log(e.key);
-    // if (e.key == 'ArrowRight') {
-    //   console.log('rechts');
-    // }
-    //
-    // else if (e.key == 'ArrowLeft'){
-    //   console.log('links');
-    // }
+    if (e.key == 'ArrowRight') {
+      console.log('rechts');
+    }
+
+    else if (e.key == 'ArrowLeft'){
+      console.log('links');
+    }
 });
