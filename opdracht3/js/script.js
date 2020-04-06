71c862cad194d71beda064cd72624f00f044ca47
ThomasStevens <58775290+thomas-stevens@users.noreply.github.com>
2020-04-06 20:14:52 +0200

    Reviews gefixt, nu filter nog

--- opdracht3/js/script.js
+++ opdracht3/js/script.js
@@ -29,44 +29,57 @@ function showMovies(jsonObj) {
   const movies = jsonObj;
 
   for (let i = 0; i < movies.length; i++) {
-    const myArticle = document.createElement('article');
-    const myH2 = document.createElement('h2');
-    const filmNummer = document.createElement('span');
-    const myPara1 = document.createElement('div');
-    const myPara2 = document.createElement('p');
-    const myPara3 = document.createElement('p');
-    const myImg = document.createElement('img');
-    const trailer = document.createElement('button');
-    const review = document.createElement('div');
+    let myArticle = document.createElement('article');
+    let myH2 = document.createElement('h2');
+    let myH3 = document.createElement('h3');
+    let filmNummer = document.createElement('span');
+    let myPara1 = document.createElement('div');
+    let myPara2 = document.createElement('p');
+    let myPara3 = document.createElement('p');
+    let myImg = document.createElement('img');
+    let trailer = document.createElement('button');
+    let reviews = document.createElement('ul');
 
     myH2.textContent = movies[i].title;
+    myH3.textContent = 'Reviews';
     filmNummer.textContent = movies[i].id;
     myPara1.textContent = 'Plot: ' + movies[i].simple_plot;
     myPara2.textContent = 'Genres: ' + movies[i].genres;
     myPara3.textContent = 'Release Date: ' + movies[i].release_date;
     myImg.src = movies[i].cover;
     trailer.textContent = 'Trailer';
-    review.textContent = 'Review: ' + movies[i].reviews;
 
-    for (let t = 1; t < movies[i].reviews.length; t++) {
-      let reviewScore = movies[i].reviews[t].score;
-      // en dan nog code om de review aan je film teo te voegen
+    for (let t = 0; t < movies[i].reviews.length; t++) {
+      let review = document.createElement('li');
+		let reviewScore = movies[i].reviews[t].score;
+      review.textContent = reviewScore;
+		reviews.appendChild(review);
     }
 
     myPara1.classList.add("plot");
     trailer.classList.add("knop");
-    review.classList.add("review")
+    reviews.classList.add("reviews");
+    myH3.classList.add("review-title");
 
     myArticle.appendChild(myH2);
+    myArticle.appendChild(myH3);
     myArticle.appendChild(filmNummer);
     myArticle.appendChild(myPara1);
     myArticle.appendChild(myPara2);
     myArticle.appendChild(myPara3);
     myArticle.appendChild(myImg);
     myArticle.appendChild(trailer);
-    myArticle.appendChild(review);
+    myArticle.appendChild(reviews);
 
     section.appendChild(myArticle);
 
   }
 }
+
+var filterblok = document.querySelector(".filters");
+
+filterblok.addEventListener('change', function (event) {
+
+document.body.setAttribute("data-filter", event.target.id);
+
+});
